ce0fe70370870ac0c1932cf45f425edeef3f0b3e
Aakash Japi <aakashjapi@gmail.com>
2013-11-23 17:34:53 -0800

    Fixing Server File

--- server.py
+++ server.py
@@ -3,34 +3,28 @@ app = Flask(__name__)
 import urllib
 import os
 
+SERVER_PORT = 56555
 
 @app.route('/', methods =['POST'])
 def decrypt():
 	file1 = extract_image(request.form['url_id'])
-	#encrypt = os.system("go run {0}".format(filename))()
-	proc = subprocess.Popen(["go", "run", "steg.go", "-d", file1], stdout=subprocess.PIPE)
+	proc = subprocess.Popen(["./steg","-d", file1], stdout=subprocess.PIPE)
 	message = proc.stdout.readline()
 	signature = "__%%__$$__"
 	if signature == message[0:len(signature)]:
 		message = message[len(signature):]
-		#return send_string_back(message)
 		return message
 	else:
 		return None
 
-#def send_string_back(msg):
-#	return msg
-
 def extract_image(link):
-	#urllib.urlretrieve(link, "/aakashjapi/dropbox/my_projects")
 	f = open("test.png", "wb")
 	f.write(urllib.urlopen(link).read())
 	f.close()
 	return f.name
 
-
 if __name__ == '__main__':
-    app.run()
+    app.run(port=int(SERVER_PORT))
 
 
 
