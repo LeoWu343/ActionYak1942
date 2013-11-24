aa2e1e5c662d66a3dcbe8f200576b03867f6e282
Aakash Japi <aakashjapi@gmail.com>
2013-11-24 02:47:25 -0800

    Imgur implemented

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, Response, json, session
 import flask
-#import tweepy
-app = Flask(__name__)
 import urllib
 import os
 import subprocess
-#from twitter.api import Twitter
-#import oauth2 as ouath
 import base64
-#from flask_oauth import oauth
+import json
+import requests
+from base64 import b64encode
+import pyimgur
 
+app = Flask(__name__)
 SERVER_PORT = 56555
 SIGNATURE = "__%%__$$__"
 KEY_DELIMETER = "|||"
@@ -54,11 +54,40 @@ def action():
 		file1 = extract_image(request.form["url_id"])
 		proc = subprocess.Popen(["./steg","-e", file1, file1, msg], stdout=subprocess.PIPE)
 		#message = proc.stdout.readline()
-		with open(file1, "rb") as f:
-			data = f.read()
-    		data.encode("base64")
-		send_file(file1)
+		# with open(file1, "rb") as f:
+		# 	data = f.read()
+  		#	data.encode("base64")
+		# send_file(file1)
+		link1 = imgur(file1)
+		return link1
 #---------Tweet-------------------
+
+def imgur(file1):
+	# client_id = '435af258a810ee9'
+	# headers = {"Authorization": "Client-ID my-client-id"}
+	# api_key = 'my-api-key'
+	# url = "https://api.imgur.com/3/upload.json"
+	# j1 = requests.post(url, headers = headers,
+	#     data = {
+	#         'key': api_key, 
+	#         'image': b64encode(open('file1','rb').read()),
+	#         'type': 'base64',
+	#         'name': '1.jpg',
+	#         'title': 'Picture no. 1'
+	#     })
+	client_id = '435af258a810ee9'
+	client_secret = '30c186991750dbe5619579ad8f8845ad9a224232'
+	url = 'https://api.imgur.com/3/upload.json'
+	path = file1
+	#headers = {'Authorization':'Client-ID {0}'.format(client_id)}
+	im = pyimgur.Imgur(client_id, client_secret)
+	uploaded_image = im.upload_image(path, "Anonymous upload with PyImgur")
+	print(uploaded_image.title)
+	print(uploaded_image.link)
+	print(uploaded_image.size)
+	print(uploaded_image.type)
+	return uploaded_image.link
+
 
 def send_file(pic):
 	return send_file(pic, mimetype='image/png')
